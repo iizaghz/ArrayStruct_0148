39342264b18e09910c80e967bc8d8cd8b94d63f1
iizaghz <sukmahawaiza@gmail.com>
2025-04-15 19:51:14 +0700

    menambahkan getline dan ignore pada code cin

--- structGetLine.cpp
+++ structGetLine.cpp
@@ -17,6 +17,24 @@ int main ()  {
     mahasiswa mhs[2];
 
 for (int n = 0;n < 2; n++) {
+    cout << "Data ke-" << n+1 << endl;
+    cout << "Masukkan NIM = " ;
+    cin >> mhs[n].nim;
+    cin.ignore();
+    cout << "Masukkan NAMA = " ;
+    getline (cin , mhs[n].nama);
+    cin >> mhs[n].nama;
+    cout << "Masukkan ALAMAT DESA = " ;
+    cin >> mhs[n].alamat.desa;
+    cout << "Masukkan ALAMAT KOTA = " ;
+    cin >> mhs[n].alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
     
+
+    cout  << "NIM = " << mhs[n].nim << endl;
+    cout  << "NAMA" << mhs[n].nim << endl;
+    cout  << "ALAMAT" << mhs[n].nim << endl;
 } 
 }
