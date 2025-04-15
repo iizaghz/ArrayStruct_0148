4c24bf90f40d180b2db6542672a0dc4b5a4becaa
iizaghz <sukmahawaiza@gmail.com>
2025-04-15 19:47:01 +0700

    menambahkan object struct mahasiswa dengan ukuran array 2

---
+++ structGetLine.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa {
+   string nim;
+   string nama;
+   detailAlamat alamat;
+};
+
+int main ()  {
+    // membuat object struct mahasiswa 
+    mahasiswa mhs[2];
+
+
+}
