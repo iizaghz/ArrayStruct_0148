169d10848b356845cac17410108cb0893cb26f3c
iizaghz <sukmahawaiza@gmail.com>
2025-04-15 19:42:19 +0700

    menampilkan struct data mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -15,6 +15,15 @@ mhs.nim = "2022014000";
 mhs.nama = "Abra";
 mhs.alamat = "Kasihan";
 
+cout << "Masukkan NIM = " ;
+cin >> mhs.nim;
+cout << "Masukkan NAMA" ;
+cin >> mhs.nama;
+cout << "Masukkan ALAMAT" ;
+cin >> mhs.alamat;
 
+cout  << "NIM = " << mhs.nim << endl;
+cout  << "NAMA" << mhs.nim << endl;
+cout  << "ALAMAT" << mhs.nim << endl;
 
 }
